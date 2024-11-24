982e3e8a42507d9799358f7c6382a3e0f283e848
DinukaIshara <dinukaphoto692@gmail.com>
2024-11-24 10:14:09 +0530

    Create Project

---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
